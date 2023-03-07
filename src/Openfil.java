1e5a4dd07343e88ad0f3067c8d2644a04f22b240
Elias <sjoberg.sjoberg@elev.ga.ntig.se>
2023-03-07 14:32:51 +0100

    hej

--- src/Openfil.java
+++ src/Openfil.java
@@ -8,20 +8,20 @@ import java.util.Scanner;
 public class Openfil {
 
 
-    public void fileReader() {
-        Scanner filscanner =new Scanner(System.in);
-        File myFile = new File(filscanner + ".txt");
-        try {
-            Scanner myScan = new Scanner(myFile);
-            while (myScan.hasNextLine()) {
-                String data = myScan.nextLine();
-                System.out.println(data);
+        public void fileReader() {
+            Scanner filscanner =new Scanner(System.in);
+            File myFile = new File(filscanner + ".txt");
+            try {
+                Scanner myScan = new Scanner(myFile);
+                while (myScan.hasNextLine()) {
+                    String data = myScan.nextLine();
+                    System.out.println(data);
+                }
+                myScan.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("could not find file");
+                e.printStackTrace();
             }
-            myScan.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("could not find file");
-            e.printStackTrace();
-        }
 
-    }
+        }
 }
